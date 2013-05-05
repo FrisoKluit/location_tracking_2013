d9e6a454a1e89dc5c32dcffcc21bef704a14f001
frisokluit <friso.kluit@gmail.com>
2013-05-05 16:54:06 +0800

    Sunday

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
-
+  , path = require('path')
+  , gcm = require('node-gcm');
 var app = express();
 
 // all environments
