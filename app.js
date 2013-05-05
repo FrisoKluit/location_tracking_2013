7ddd7e04751065c5c1390321382481a2d175bf70
frisokluit <friso.kluit@gmail.com>
2013-05-05 22:16:45 +0800

    Sunday

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ app.get('/users', user.list);
 app.get('/mobile', mobile.index);
 app.post('/newloc', routes.newloc);
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
